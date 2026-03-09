7d57749e39fa0fc4bea4a363c2947d174d297bb0
błizzård <65140356+belizairebr25@users.noreply.github.com>
2026-03-09 14:23:45 -0400

    added speaker functionality

--- 1166RTOS.ino
+++ 1166RTOS.ino
@@ -4,6 +4,7 @@ int lowPowerPin = 6;
 int state = -1;
 int StartTime = 1;
 int mode = 1; //mode 1: regular, mode 2: LowPower, mode 3: cardiac, mode 10: both issues
+int tonePin = 1
 
 void cycle(){
   if(millis() > StartTime + 1000 / mode){ // 1 second divided by mode. 50% duty cycle
@@ -11,8 +12,10 @@ void cycle(){
     state = state * -1;
     if(state == 1){
       digitalWrite(ledPin, HIGH);
+      tone(tonePin, 880);
     } else {
       digitalWrite(ledPin, LOW);
+      noTone(tonePin);
     }
   } 
 }
@@ -39,13 +42,16 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(cardiacPin, INPUT_PULLUP);
   pinMode(lowPowerPin, INPUT_PULLUP);
+  pinMode(tonePin, OUTPUT);
 }
 
 void loop() {
-  // put your main ode here, to run repeatedly:
-  //interval = output mode; 3 modes: normal, Cardiac Alert, Low Voltage
+  // put your main code here, to run repeatedly:
+  
+  //interval = output mode; 3 modes: normal, Cardiac Alert, Low Voltage, both
   cycle();
   if(checkCardiac() == 1){
+    
     mode = 3;
   }else if(checkLowPower() == 1){
     mode = 2;    
